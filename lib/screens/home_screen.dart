f6de5b95ae12bb2732c94ba4dbeea959276d1a40
Parth <parthtagalpallewar123@gmail.com>
2023-04-30 22:12:53 +0530

    Main Screen Data for crypto fetched

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables
-
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:web_socket_channel/io.dart';
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,37 +10,57 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> data = [];
+  late IOWebSocketChannel channel;
+  late List<dynamic> tickers;
+
+  @override
+  void initState() {
+    super.initState();
+    streamListener();
+
+  }
+
+  streamListener() {
+    channel = IOWebSocketChannel.connect('wss://stream.binance.com:9443/ws/!ticker@arr');
+    channel.stream.listen((message) {
+      setState(() {
+        tickers = jsonDecode(message);
+        print(tickers.length);
+        // for (final ticker in tickers) {
+        //   final symbol = ticker['s'];
+        //   final price = ticker['c'];
+        //   final volume = ticker['v'];
+        //   final change = ticker['P'];
+        //   print('$symbol: $price ($change%)');
+        // }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('CryptoVision'),
-      ),
-      body: ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (context, index) {
-          final user = data[index];
-          return ListTile(
-            title: Text(user['email']),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: fetchData,
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+          child: ListView.builder(
+              itemBuilder: (BuildContext context, int position){
+                final ticker = tickers[position];
+
+                if(ticker != null){
+                  final String symbol = ticker['s'].toString() ?? "DUMMY";
+                  return ListTile(
+                    title: Text(symbol),
+                    trailing: Text(ticker['c'].toString()),
+                  );
+                }else {
+                  return const ListTile(
+                    title: Text("NULL data"),
+                  );
+                }
+
+              }
+          )
       ),
     );
-  }
-
-  void fetchData() async {
-    const url = 'https://randomuser.me/api/?results=10';
-    final uri = Uri.parse(url);
-    final response = await http.get(uri);
-    final body = response.body;
-    final json = jsonDecode(body);
-    setState(() {
-      data = json['results'];
-    });
   }
 }
