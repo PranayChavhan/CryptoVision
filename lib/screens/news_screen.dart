4ea951a97ec7586762dd16c3243e40f3f5e02a3a
PranayChavhan <pranay21chavhan@outlook.com>
2023-05-04 00:40:13 +0530

    newsScreen updated

--- lib/screens/news_screen.dart
+++ lib/screens/news_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cryptovision/models/Article.dart';
 import 'package:flutter/material.dart';
-import '../components/myappbar.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import '../models/News.dart';
 import 'Widgets.dart';
 
@@ -16,46 +17,134 @@ class _NewsScreenState extends State<NewsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          centerTitle: true,
-          elevation: 0,
-          title: Text(
-            "Crypto Trends",
-            style: TextStyle(fontSize: 16),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text(
+          'Crypto News',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        body: FutureBuilder(
-            future: news.getNews(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                List<Article> data = snapshot.data!;
-                return ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    shrinkWrap: true,
-                    physics: const ClampingScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      print("data hai bhai");
-                      return NewsTile(
-                        imgUrl: data[index].urlToImage,
-                        title: data[index].title,
-                        desc: data[index].description,
-                        content: data[index].content,
-                        posturl: data[index].url,
-                      );
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.notifications,
+              size: 30.0,
+            ),
+          ),
+        ],
+      ),
+      // drawer: _buildDrawer(context),
+      body: FutureBuilder<List<Article>>(
+        future: news.getNews(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List<Article> data = snapshot.data!;
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              shrinkWrap: true,
+              physics: const ClampingScrollPhysics(),
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {},
+                  child: Card(
+                    color: Colors.grey[850]!,
+                    margin: const EdgeInsets.all(10.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 150.0,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              image: DecorationImage(
+                                image: NetworkImage(data[index].urlToImage),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 10.0),
+                          Text(
+                            data[index].title,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 10.0),
+                          Text(
+                            data[index].description,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          const SizedBox(height: 10.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                _getDate(data[index].publishedAt),
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0,
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () async {
+                                  if (await canLaunchUrlString(
+                                      data[index].url)) {
+                                    await launchUrlString(data[index].url);
+                                  } else {
+                                    throw 'Could not launch $data[index].url)';
+                                  }
+                                },
+                                child: const Text(
+                                  'Read More',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+              ),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
 
-                      return const Text("hello");
-                    });
-              }
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text(snapshot.error.toString()),
-                );
-              } else {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            }));
+  String _getDate(String dateStr) {
+    DateTime date = DateTime.parse(dateStr);
+    String formattedDate =
+        '${date.day.toString().padLeft(2, '0')}/${date.month.toString().padLeft(2, '0')}/${date.year.toString()}';
+    String formattedTime =
+        '${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}';
+    return '$formattedDate $formattedTime';
   }
 }
