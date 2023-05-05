4cbbccfa6c9500bcbf6b7d9a66281b1dfec68528
PranayChavhan <pranay21chavhan@outlook.com>
2023-05-05 09:36:47 +0530

    __

--- lib/models/coinModel.dart
+++ lib/models/coinModel.dart
@@ -15,11 +15,25 @@ class CoinModel {
     required this.currentPrice,
     required this.marketCap,
     required this.marketCapRank,
+    // required this.fullyDilutedValuation,
     required this.totalVolume,
     required this.high24H,
     required this.low24H,
     required this.priceChange24H,
+    // required this.priceChangePercentage24H,
+    // required this.marketCapChange24H,
     required this.marketCapChangePercentage24H,
+    // required this.circulatingSupply,
+    // required this.totalSupply,
+    // required this.maxSupply,
+    // required this.ath,
+    // required this.athChangePercentage,
+    // required this.athDate,
+    // required this.atl,
+    // required this.atlChangePercentage,
+    // required this.atlDate,
+    // this.roi,
+    // required this.lastUpdated,
     required this.sparklineIn7D,
   });
 
@@ -30,28 +44,55 @@ class CoinModel {
   double currentPrice;
   int marketCap;
   int marketCapRank;
+  // int fullyDilutedValuation;
   double totalVolume;
   double high24H;
   double low24H;
   double priceChange24H;
   // double priceChangePercentage24H;
+  // int marketCapChange24H;
   double marketCapChangePercentage24H;
+  // int circulatingSupply;
+  // int totalSupply;
+  // int maxSupply;
+  // int ath;
+  // double athChangePercentage;
+  // DateTime athDate;
+  // double atl;
+  // double atlChangePercentage;
+  // DateTime atlDate;
+  // dynamic roi;
+  // DateTime lastUpdated;
   SparklineIn7D sparklineIn7D;
 
   factory CoinModel.fromJson(Map<String, dynamic> json) => CoinModel(
-        id: json["id"],
-        symbol: json["symbol"],
-        name: json["name"],
+        id: json["s"],
+        symbol: json["k"]["s"],
+        name: json["k"]["s"],
         image: json["image"],
         currentPrice: json["current_price"].toDouble(),
         marketCap: json["market_cap"],
         marketCapRank: json["market_cap_rank"],
+        // fullyDilutedValuation: json["fully_diluted_valuation"],
         totalVolume: json["total_volume"].toDouble(),
         high24H: json["high_24h"].toDouble(),
         low24H: json["low_24h"].toDouble(),
         priceChange24H: json["price_change_24h"]?.toDouble(),
+        // priceChangePercentage24H: json["price_change_percentage_24h"]?.toDouble(),
+        // marketCapChange24H: json["market_cap_change_24h"],
         marketCapChangePercentage24H:
             json["market_cap_change_percentage_24h"]?.toDouble(),
+        // circulatingSupply: json["circulating_supply"],
+        // totalSupply: json["total_supply"],
+        // maxSupply: json["max_supply"],
+        // ath: json["ath"],
+        // athChangePercentage: json["ath_change_percentage"]?.toDouble(),
+        // athDate: DateTime.parse(json["ath_date"]),
+        // atl: json["atl"]?.toDouble(),
+        // atlChangePercentage: json["atl_change_percentage"]?.toDouble(),
+        // atlDate: DateTime.parse(json["atl_date"]),
+        // roi: json["roi"],
+        // lastUpdated: DateTime.parse(json["last_updated"]),
         sparklineIn7D: SparklineIn7D.fromJson(json["sparkline_in_7d"]),
       );
 
@@ -63,11 +104,25 @@ class CoinModel {
         "current_price": currentPrice,
         "market_cap": marketCap,
         "market_cap_rank": marketCapRank,
+        // "fully_diluted_valuation": fullyDilutedValuation,
         "total_volume": totalVolume,
         "high_24h": high24H,
         "low_24h": low24H,
         "price_change_24h": priceChange24H,
+        // "price_change_percentage_24h": priceChangePercentage24H,
+        // "market_cap_change_24h": marketCapChange24H,
         "market_cap_change_percentage_24h": marketCapChangePercentage24H,
+        // "circulating_supply": circulatingSupply,
+        // "total_supply": totalSupply,
+        // "max_supply": maxSupply,
+        // "ath": ath,
+        // "ath_change_percentage": athChangePercentage,
+        // "ath_date": athDate.toIso8601String(),
+        // "atl": atl,
+        // "atl_change_percentage": atlChangePercentage,
+        // "atl_date": atlDate.toIso8601String(),
+        // "roi": roi,
+        // "last_updated": lastUpdated.toIso8601String(),
         "sparkline_in_7d": sparklineIn7D.toJson(),
       };
 }
